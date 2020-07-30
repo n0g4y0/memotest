b8fe356dbe0b482f1728cd135d981436bca30358
n0g4y0 <n0g4y0@gmail.com>
2020-07-30 06:19:58 -0400

    prueba de cypres
    prueba de cypress, introduccion

---
+++ cypress/integration/prueba.js
@@ -0,0 +1,19 @@
+/// <reference types="Cypress" />
+
+//recuerden correr http-server primero!
+const URL = '127.0.0.1:8080';
+const NUMERO_CUADROS = 12;
+
+context('Memotest', () => { 
+
+  before(() => {
+    cy.visit(URL);
+  });
+
+  it('se asegura que haya un tablero con cuadros', () => {
+      //document.QuerySelector('#tablero').QuerySelectorAll('.cuadro')
+    cy.get('#tablero').find('.cuadro').should('have.length', NUMERO_CUADROS);
+  });
+
+
+});
